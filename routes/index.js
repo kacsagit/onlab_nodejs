e9af9739e13c3913253ade29388ecd3d263b9ece
kacsagit <katajona@gmail.com>
2017-02-11 23:38:28 +0100

    cloud

--- routes/index.js
+++ routes/index.js
@@ -61,7 +61,7 @@ app.get('/ab*cd', function (req, res) {
     res.send('Page Pattern Match');
 });
 
-var server = server.listen(80, function () {
+var server = server.listen(8081, function () {
 
     var host = server.address().address;
     var port = server.address().port;
@@ -69,11 +69,13 @@ var server = server.listen(80, function () {
     console.log("Example app listening at http://%s:%s", host, port)
 });
 
+var server   =   app.listen(3000,function(){
+    console.log("We have started our server on port 3000");
+});
+
 io.on('connection',function(socket){
     console.log("A user is connected");
 });
 
 
-
-module.exports = io;
 module.exports = express.Router();
