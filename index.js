8ec52d7e04b7ad12a72a866b0ffebe9b0f53ac5f
kacsagit <katajona@gmail.com>
2017-02-13 23:08:12 +0100

    heroku

---
+++ index.js
@@ -0,0 +1,79 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var mysql = require('mysql');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+var connection = mysql.createConnection({
+
+ host: 'us-cdbr-iron-east-04.cleardb.net',
+ user: 'bea824782eebcb',
+ password: 'c8e130b4',
+ database: 'heroku_9cfec1cbd3c0c63'
+ });
+
+ connection.connect(function (error) {
+ if (!!error) {
+ console.log(error);
+ } else {
+ console.log("Connected");
+ }
+
+ });
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+    console.log("Got a GET request for the homepage");
+
+     connection.query("Select * from mySampleTable", function (error, rows, fields) {
+     if (!!error) {
+     console.log('Error in query' + error);
+     } else {
+     console.log("Success");
+     console.log(rows[0].Name);
+     res.json(rows);
+     }
+     });
+
+});
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+    console.log("Got a POST request for the homepage");
+    res.send('Hello POST');
+});
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+    console.log("Got a DELETE request for /del_user");
+    res.send('Hello DELETE');
+});
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+    console.log("Got a GET request for /list_user");
+    res.send('Page Listing');
+});
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function (req, res) {
+    console.log("Got a GET request for /ab*cd");
+    res.send('Page Pattern Match');
+});
+
+
+app.get('/cool', function(request, response) {
+    response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
