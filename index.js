56746916b5bf83e0f0c5fbc07fa650f4b313cc0b
kacsagit <katajona@gmail.com>
2017-04-24 00:50:35 +0200

    upload image

--- index.js
+++ index.js
@@ -335,7 +335,7 @@ app.get('/api/get',
             "inner join friends f on o.ownerid=f.user_id2 " +
             "where f.user_id1=? " +
             "and o.done=0" +
-            "and f.id<>?", [req.user.id,req.user.id], function (error, rows, fields) {
+            "and o.ownerid<>?", [req.user.id,req.user.id], function (error, rows, fields) {
             if (!!error) {
                 console.log('Error in query' + error);
             } else {
@@ -617,8 +617,14 @@ app.get('/api/imagebyid', function (req, res) {
 
 app.get('/api/image', function (req, res) {
     var id = req.query.image;
-    res.sendfile(req.query.image);
+    res.sendfile(req.query.image).resize(200, 200);
 });
+
+app.get('/image', function (req, res) {
+    var id = req.query.image;
+    res.sendfile(req.query.image).resize(200, 200);
+});
+
 
 
 app.get('/cool', function (request, response) {
